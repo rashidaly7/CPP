fbefcbd9e45377b2491bb06c1b1eb1e12e4756b9
rashid ali <rashidaly7>
2026-07-27 15:26:19 +0530

    changese

--- Common_errore.cpp
+++ Common_errore.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
   int x = 5
   cout << x;
+  cout << b;
+  int c = "Helllo";
   return 0;
 }
 // error: expected ',' or ';' before 'cout'
+// error: 'myVar' was not declared in this scope
+// error: invalid conversion from 'const char*' to 'int'
